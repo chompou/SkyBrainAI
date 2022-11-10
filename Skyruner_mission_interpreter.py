4aa6ba4f7bd6d1cf54516b9e2c34e10b9e63a874
pistolav <78804567+pistolav@users.noreply.github.com>
2022-11-10 17:15:18 +0100

    Runner file and custom env added

---
+++ Skyruner_mission_interpreter.py
@@ -0,0 +1,80 @@
+def translate_action(action):
+    forward = action == 0
+    backward = action == 1
+    left = action == 2
+    right = action == 3
+    cam_pitch_l = action == 4
+    cam_pitch_r = action == 5
+    cam_yaw_up = action == 6
+    cam_yaw_down = action == 7
+
+    return [
+        1 if forward else 2 if backward else 0,             # 0
+        1 if left else 2 if right else 0,                   # 1
+        0,                                                  # 2
+        11 if cam_yaw_up else 13 if cam_yaw_down else 12,   # 3
+        11 if cam_pitch_l else 13 if cam_pitch_r else 12,   # 4
+        0,                                                  # 5
+        0,                                                  # 6
+        0,                                                  # 7
+    ]
+
+
+class Mission:
+    def __init__(self, explore=False, obs_simplify=True, mine=False, survival=False, collect_amount=None,
+                 per_item_reward=None, episode_length=1000, env=None, prev_distance=0):
+        if per_item_reward is None:
+            per_item_reward = []
+        if collect_amount is None:
+            collect_amount = []
+        self.obs_simplify = obs_simplify
+        self.explore = explore
+        self.mine = mine
+        self.survival = survival
+        self.per_item_reward = per_item_reward
+        self.collect_amount = collect_amount
+        self.delta = ({}, {}, {}, {'distance_travelled_cm': prev_distance})
+        self.episode = 0
+        self.episode_length = episode_length
+        self.env = env
+
+    def eval(self, obs, reward, done, info):
+        if reward is None:
+            reward = 0
+        self.episode += 1
+        if self.obs_simplify:
+            obs = obs.get('rgb')[:, :60, :60]
+        if self.episode >= self.episode_length:
+            done = True
+        if self.survival:
+            if info.get('is_dead'):
+                done = True
+            if info.get('living_death_event_fired'):
+                done = True
+        if self.explore:
+            new = info.get('distance_travelled_cm') if info.get('distance_travelled_cm') is not None else 0
+            old = self.delta[3].get('distance_travelled_cm') if self.delta[3].get(
+                    'distance_travelled_cm') is not None else 0
+            reward += new - old
+        self.delta = (obs, reward, done, info)
+        return obs, reward, done, info
+
+    def reset(self):
+        self.env.reset()
+        self.__init__(explore=self.explore, obs_simplify=self.obs_simplify, mine=self.mine, survival=self.survival,
+                      collect_amount=self.collect_amount,
+                      per_item_reward=self.per_item_reward, episode_length=self.episode_length, env=self.env,
+                      prev_distance=self.delta[3].get('distance_travelled_cm') if self.delta[3].get('distance_travelled_cm') is not None else 0)
+        for i in range(5):
+            self.env.step(translate_action(100))
+
+    def step(self, action):
+        _, __, ___, ____ = self.env.step(action)
+        return self.eval(_, __, ___, ____)
+
+    def quit(self):
+        self.env.__exit__()
+
+    def stepNum(self, num):
+        action = translate_action(num)
+        return self.step(action)
